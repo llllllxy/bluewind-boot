e44b82a39e87850fc46c59db15fded3fa997e3d8
liuxingyu01 <184974699@qq.com>
2021-08-19 10:09:00 +0800

    JacksonConfig增加注释

--- src/main/java/com/liuxingyu/meco/configuration/JacksonConfig.java
+++ src/main/java/com/liuxingyu/meco/configuration/JacksonConfig.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,6 +33,14 @@ public class JacksonConfig {
     @ConditionalOnMissingBean(ObjectMapper.class)
     public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+
+        // 让返回json数据中的Long类型转换为String（暂时用不到，先注释掉）
+        // SimpleModule simpleModule = new SimpleModule();
+        // simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        // simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        // objectMapper.registerModule(simpleModule);
+
+        // 让返回的json数据中的null转为""字符串
         objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
             @Override
             public void serialize(Object o, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
