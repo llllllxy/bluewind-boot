53ac3be643e5f465c69220c524d76e921c6ece4c
liuxingyu01 <184974699@qq.com>
2021-08-28 16:22:28 +0800

    新版定时器代码入库

---
+++ src/main/java/com/liuxingyu/meco/sys/sysjoblog/service/SysJobLogServiceImpl.java
@@ -0,0 +1,93 @@
+package com.liuxingyu.meco.sys.sysjoblog.service;
+
+import com.liuxingyu.meco.sys.sysjoblog.entity.SysJobLog;
+import com.liuxingyu.meco.sys.sysjoblog.mapper.SysJobLogMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 定时任务调度日志信息 服务层
+ *
+ * @author liuxingyu01
+ * @date 2021-08-27-16:31
+ **/
+@Service
+public class SysJobLogServiceImpl implements SysJobLogService {
+
+    @Autowired
+    private SysJobLogMapper jobLogMapper;
+
+    /**
+     * 获取quartz调度器日志的计划任务
+     *
+     * @param jobLog 调度日志信息
+     * @return 调度任务日志集合
+     */
+    @Override
+    public List<SysJobLog> selectJobLogList(SysJobLog jobLog) {
+        return jobLogMapper.selectJobLogList(jobLog);
+    }
+
+    /**
+     * 通过调度任务日志ID查询调度信息
+     *
+     * @param jobLogId 调度任务日志ID
+     * @return 调度任务日志对象信息
+     */
+    @Override
+    public SysJobLog selectJobLogById(Long jobLogId) {
+        return jobLogMapper.selectJobLogById(jobLogId);
+    }
+
+    /**
+     * 新增任务日志
+     *
+     * @param jobLog 调度日志信息
+     */
+    @Override
+    public void addJobLog(SysJobLog jobLog) {
+        jobLogMapper.insertJobLog(jobLog);
+    }
+
+    /**
+     * 批量删除调度日志信息
+     *
+     * @param logIds 需要删除的数据ID
+     * @return 结果
+     */
+    @Override
+    public int deleteJobLogByIds(Long[] logIds) {
+        return jobLogMapper.deleteJobLogByIds(logIds);
+    }
+
+    /**
+     * 删除任务日志
+     *
+     * @param jobId 调度日志ID
+     */
+    @Override
+    public int deleteJobLogById(Long jobId) {
+        return jobLogMapper.deleteJobLogById(jobId);
+    }
+
+    /**
+     * 清空任务日志
+     */
+    @Override
+    public void cleanJobLog() {
+        jobLogMapper.cleanJobLog();
+    }
+
+
+    /**
+     * 根据jobId获取执行日志
+     * @param jobId
+     * @return
+     */
+    @Override
+    public List<SysJobLog> getLogByJobId(String jobId) {
+        return jobLogMapper.getLogByJobId(jobId);
+    }
+}
