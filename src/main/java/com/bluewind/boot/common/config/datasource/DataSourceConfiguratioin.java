7ed293a3d79b65e93ee2534c4d1c971d47208526
liuxingyu01 <184974699@qq.com>
2021-11-28 19:49:12 +0800

    辅数据源增加开关

--- src/main/java/com/bluewind/boot/common/config/datasource/DataSourceConfiguratioin.java
+++ src/main/java/com/bluewind/boot/common/config/datasource/DataSourceConfiguratioin.java
@@ -4,10 +4,10 @@ import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import com.bluewind.boot.common.enums.DataSourceType;
 import com.bluewind.boot.common.config.datasource.properties.DruidProperties;
+import com.bluewind.boot.common.utils.spring.SpringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -28,7 +28,7 @@ public class DataSourceConfiguratioin {
     Logger logger = LoggerFactory.getLogger(DataSourceConfiguratioin.class);
 
     /**
-     * 主数据源 Master data source.
+     * 主数据源 Master datasource.
      *
      * @param druidProperties Druid 配置文件
      * @return 主数据源
@@ -43,13 +43,14 @@ public class DataSourceConfiguratioin {
 
 
     /**
-     * 辅数据源 Slave data source.
+     * 辅数据源 Slave datasource.
      *
      * @param druidProperties Druid 配置文件
      * @return 辅数据源
      */
     @Bean("slaveDataSource")
     @ConfigurationProperties(prefix = "spring.datasource.druid.slave")
+    @ConditionalOnProperty(prefix = "spring.datasource.druid.slave", name = "enabled", havingValue = "true")
     public DataSource slaveDataSource(DruidProperties druidProperties) {
         logger.info("DataSourceConfiguratioin create slave datasource...");
         DruidDataSource dataSource = DruidDataSourceBuilder.create().build();
@@ -58,7 +59,7 @@ public class DataSourceConfiguratioin {
 
 
     /**
-     * Quartz定时任务专用数据源 Quartz data source.
+     * Quartz定时任务专用数据源 Quartz datasource.
      *
      * @param druidProperties Druid 配置文件
      * @return Quartz定时任务专用数据源
@@ -77,22 +78,37 @@ public class DataSourceConfiguratioin {
      * 动态数据源入口
      *
      * @param masterDataSource 主数据源
-     * @param slaveDataSource  辅数据源
      * @return {@link RoutingDataSource}
      */
     @Bean("dataSource")
     @Primary
-    public RoutingDataSource dataSource(@Autowired @Qualifier("masterDataSource") DataSource masterDataSource,
-                                        @Autowired @Qualifier("slaveDataSource") DataSource slaveDataSource) {
+    public RoutingDataSource dataSource(DataSource masterDataSource) {
         logger.info("DataSourceConfiguratioin create routing datasource...");
         Map<Object, Object> targetDataSources = new HashMap<>();
         targetDataSources.put(DataSourceType.MASTER.name(), masterDataSource);
-        targetDataSources.put(DataSourceType.SLAVE.name(), slaveDataSource);
+        setDataSource(targetDataSources, DataSourceType.SLAVE.name(), "slaveDataSource");
         RoutingDataSource routing = new RoutingDataSource();
         routing.setTargetDataSources(targetDataSources);
         routing.setDefaultTargetDataSource(masterDataSource);
         routing.afterPropertiesSet();
         return routing;
+    }
+
+
+    /**
+     * 设置数据源
+     *
+     * @param targetDataSources 备选数据源集合
+     * @param sourceName        数据源名称
+     * @param beanName          bean名称
+     */
+    public void setDataSource(Map<Object, Object> targetDataSources, String sourceName, String beanName) {
+        try {
+            DataSource dataSource = SpringUtil.getBean(beanName);
+            targetDataSources.put(sourceName, dataSource);
+        } catch (Exception e) {
+            logger.info("DataSourceConfiguratioin slave datasource not enabled...");
+        }
     }
 
 }
