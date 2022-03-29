f2c45a4060375f817065f5fdbf84b442a08fcc4d
liuxingyu01 <184974699@qq.com>
2022-03-29 20:29:04 +0800

    增加获取UUID的js方法

--- src/main/resources/static/js/lay-config.js
+++ src/main/resources/static/js/lay-config.js
@@ -603,3 +603,33 @@ function getUrlKeyValue(key) {
     return decodeURIComponent((new RegExp('[?|&]' + key + '=' + '([^&;]+?)(&|#|;|$)').exec(location.href) || [, ""])[1].replace(/\+/g, '%20')) || null
 }
 
+/**
+ * 获取UUID
+ * @returns {string}
+ */
+function uuid() {
+    var s = [];
+    var hexDigits = "0123456789abcdef";
+    for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "-";
+
+    var uuid = s.join("");
+    return uuid;
+}
+
+/**
+ * 获取UUID-V4版本
+ * @returns {string}
+ */
+function uuidV4() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+        const r = (Math.random() * 16) | 0; // 取整
+        const v = c === "x" ? r : (r & 0x3 | 0x8);
+        return v.toString();
+    });
+}
+
