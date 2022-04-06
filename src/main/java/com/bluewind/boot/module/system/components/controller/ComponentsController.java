1eff2e366d10c20bb7df737b7432fa140e66096f
liuxingyu01 <184974699@qq.com>
2022-04-06 16:10:45 +0800

    代码包名路径完全重构，进行中，还没完全搞完

--- src/main/java/com/bluewind/boot/module/system/components/controller/ComponentsController.java
+++ src/main/java/com/bluewind/boot/module/system/components/controller/ComponentsController.java
@@ -11,138 +11,138 @@ import org.springframework.web.bind.annotation.RequestMapping;
  **/
 @Api(tags = "后台管理端--组件专区")
 @Controller
-@RequestMapping("/syscomponents")
+@RequestMapping("/components")
 public class ComponentsController {
 
 
     @GetMapping("/html404")
     public String html404() {
-        return "system/syscomponents/404";
+        return "system/components/404";
     }
 
     @GetMapping("/html500")
     public String html500() {
-        return "system/syscomponents/500";
+        return "system/components/500";
     }
 
     @GetMapping("/area")
     public String area() {
-        return "system/syscomponents/area";
+        return "system/components/area";
     }
 
     @GetMapping("/button")
     public String button() {
-        return "system/syscomponents/button";
+        return "system/components/button";
     }
 
     @GetMapping("/colorSelect")
     public String colorSelect() {
-        return "system/syscomponents/color-select";
+        return "system/components/color-select";
     }
 
     @GetMapping("/editor")
     public String editor() {
-        return "system/syscomponents/editor";
+        return "system/components/editor";
     }
 
     @GetMapping("/form")
     public String form() {
-        return "system/syscomponents/form";
+        return "system/components/form";
     }
 
     @GetMapping("/formStep")
     public String formStep() {
-        return "system/syscomponents/form-step";
+        return "system/components/form-step";
     }
 
     @GetMapping("/icon")
     public String icon() {
-        return "system/syscomponents/icon";
+        return "system/components/icon";
     }
 
     @GetMapping("/iconPicker")
     public String iconPicker() {
-        return "system/syscomponents/icon-picker";
+        return "system/components/icon-picker";
     }
 
     @GetMapping("/layer")
     public String layer() {
-        return "system/syscomponents/layer";
+        return "system/components/layer";
     }
 
     @GetMapping("/login1")
     public String login1() {
-        return "system/syscomponents/login-1";
+        return "system/components/login-1";
     }
 
     @GetMapping("/login2")
     public String login2() {
-        return "system/syscomponents/login-2";
+        return "system/components/login-2";
     }
 
     @GetMapping("/login3")
     public String login3() {
-        return "system/syscomponents/login-3";
+        return "system/components/login-3";
     }
 
     @GetMapping("/menu")
     public String menu() {
-        return "system/syscomponents/menu";
+        return "system/components/menu";
     }
 
     @GetMapping("/setting")
     public String setting() {
-        return "system/syscomponents/setting";
+        return "system/components/setting";
     }
 
     @GetMapping("/table")
     public String table() {
-        return "system/syscomponents/table";
+        return "system/components/table";
     }
 
     @GetMapping("/tableAdd")
     public String tableAdd() {
-        return "system/syscomponents/table/add";
+        return "system/components/table/add";
     }
 
     @GetMapping("/tableEdit")
     public String tableEdit() {
-        return "system/syscomponents/table/edit";
+        return "system/components/table/edit";
     }
 
     @GetMapping("/tableSelect")
     public String tableSelect() {
-        return "system/syscomponents/table-select";
+        return "system/components/table-select";
     }
 
     @GetMapping("/upload")
     public String upload() {
-        return "system/syscomponents/upload";
+        return "system/components/upload";
     }
 
     @GetMapping("/userPassword")
     public String userPassword() {
-        return "system/syscomponents/user-password";
+        return "system/components/user-password";
     }
 
     @GetMapping("/userSetting")
     public String userSetting() {
-        return "system/syscomponents/user-setting";
+        return "system/components/user-setting";
     }
 
     @GetMapping("/welcome1")
     public String welcome1() {
-        return "system/syscomponents/welcome-1";
+        return "system/components/welcome-1";
     }
 
     @GetMapping("/welcome2")
     public String welcome2() {
-        return "system/syscomponents/welcome-1";
+        return "system/components/welcome-1";
     }
 
     @GetMapping("/welcome3")
     public String welcome3() {
-        return "system/syscomponents/welcome-3";
+        return "system/components/welcome-3";
     }
 
 }
