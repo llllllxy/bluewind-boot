7b3e874e8dcc16de72874dbc8d962bc9d56d43c2
liuxingyu01 <184974699@qq.com>
2022-04-06 11:00:12 +0800

    增加级联选择器模块

--- src/main/resources/static/js/lay-config.js
+++ src/main/resources/static/js/lay-config.js
@@ -73,7 +73,7 @@ layui.use(['nprogress', 'layer'], function () {
 
 
 /**
- * 搜索信息，展开，收起
+ * 搜索信息框，展开，收起
  */
 $('legend').click(function () {
     $('#search-div').slideToggle('1000');
@@ -81,11 +81,12 @@ $('legend').click(function () {
 
 
 /**
- * 设置Layui Table点击行时选中复选框
+ * 配置Layui-Table点击行时选中复选框
  */
 $(document).on("click", ".layui-table-body table.layui-table tbody tr", function () {
     var index = $(this).attr('data-index');
     var tableBox = $(this).parents('.layui-table-box');
+    var tableDiv;
     // 存在固定列
     if (tableBox.find(".layui-table-fixed.layui-table-fixed-l").length > 0) {
         tableDiv = tableBox.find(".layui-table-fixed.layui-table-fixed-l");
@@ -97,11 +98,10 @@ $(document).on("click", ".layui-table-body table.layui-table tbody tr", function
         checkCell.click();
     }
 });
-//对td的单击事件进行拦截停止，防止事件冒泡再次触发上述的单击事件（Table的单击行事件不会拦截，依然有效）
+// 对td的单击事件进行拦截停止，防止事件冒泡再次触发上述的单击事件（Table的单击行事件不会拦截，依然有效）
 $(document).on("click", "td div.laytable-cell-checkbox div.layui-form-checkbox", function (e) {
     e.stopPropagation();
 });
-
 
 
 /**
@@ -118,9 +118,6 @@ $.ajaxSetup({
         }
     }
 } );
-
-
-
 
 
 /**
