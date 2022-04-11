72b2acac78853aa6fda789540385fbc9de36621f
liuxingyu01 <184974699@qq.com>
2022-04-12 00:37:20 +0800

    提高首页加载速度，设置loadingTime默认为0

--- src/main/resources/static/js/lay-module/layuimini/miniAdmin.js
+++ src/main/resources/static/js/lay-module/layuimini/miniAdmin.js
@@ -38,7 +38,7 @@ layui.define(["jquery", "miniMenu", "element","miniTab", "miniTheme"], function 
             options.bgColorDefault = options.bgColorDefault || 0;
             options.multiModule = options.multiModule || false;
             options.menuChildOpen = options.menuChildOpen || false;
-            options.loadingTime = options.loadingTime || 1;
+            options.loadingTime = options.loadingTime || 0;
             options.pageAnim = options.pageAnim || false;
             options.maxTabNum = options.maxTabNum || 20;
             options.isHideOpenMenu = !options.isHideOpenMenu ? options.isHideOpenMenu : true;
