7ee609ca038b1a9d3852a1f2745f096a8df09a6a
liuxingyu01 <184974699@qq.com>
2022-04-22 13:33:18 +0800

    优化：SpringContextUtil的注释，更加清晰明了

--- src/main/java/com/bluewind/boot/common/utils/spring/SpringContextUtil.java
+++ src/main/java/com/bluewind/boot/common/utils/spring/SpringContextUtil.java
@@ -6,10 +6,37 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 /**
  * @author liuxingyu01
  * @date 2020-11-10-13:48
  * @description 动态获取Spring Bean工具类
+ * @demo1
+ *          private static OrganToolMapper organToolMapper;
+ *          private static OrganToolMapper getOrganToolService() {
+ *             if (organToolMapper == null) {
+ *                Object bean = SpringContextUtil.getBean("organToolMapper");
+ *                if (bean == null) {
+ *                    log.error("organToolMapper bean is null!");
+ *                }
+ *                organToolMapper = (OrganToolMapper) bean;
+ *            }
+ *            return organToolMapper;
+ *         }
+ * @demo2
+ *      private static ConvertUtilsService ConvertUtilsService; // ConvertUtilsService是接口层
+ *      private static ConvertUtilsService getConvertUtilsServiceBean() {
+ *         if (ConvertUtilsService == null) {
+ *             ConvertUtilsService bean = SpringContextUtil.getBean(ConvertUtilsService.class);
+ *             if (bean == null) {
+ *                 logger.error("ConvertUtilsService bean is null");
+ *             }
+ *             ConvertUtilsService = bean;
+ *             return ConvertUtilsService;
+ *         }
+ *         return ConvertUtilsService;
+ *      }
  **/
 @Component
 @Deprecated
@@ -17,12 +44,13 @@ public class SpringContextUtil implements ApplicationContextAware {
     private static ApplicationContext applicationContext = null;
 
     public SpringContextUtil() {
+        super();
     }
 
     @Override
-    public void setApplicationContext(ApplicationContext arg0) throws BeansException {
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
         if (applicationContext == null) {
-            applicationContext = arg0;
+            applicationContext = context;
         }
     }
 
@@ -31,31 +59,8 @@ public class SpringContextUtil implements ApplicationContextAware {
     }
 
 
-    public static void setAppCtx(ApplicationContext webAppCtx) {
-        if (webAppCtx != null) {
-            applicationContext = webAppCtx;
-        }
-    }
-
     /**
-     * 通过class获取Bean
-     *
-     * @param clazz
-     * @param <T>
-     * @return
-     */
-    public static <T> T getBean(Class<T> clazz) {
-        return getApplicationContext().getBean(clazz);
-    }
-
-
-    public static <T> T getBean(String name, Class<T> clazz) throws ClassNotFoundException {
-        return getApplicationContext().getBean(name, clazz);
-    }
-
-    /**
-     * 通过beanName获取Bean
-     *
+     * 通过名称获取Bean
      * @param beanName
      * @return
      */
@@ -64,22 +69,62 @@ public class SpringContextUtil implements ApplicationContextAware {
     }
 
 
+    /**
+     * 根据class类型获取bean
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        T t = null;
+        Map<String, T> map = getApplicationContext().getBeansOfType(clazz);
+        for (Map.Entry<String, T> entry : map.entrySet()) {
+            t = entry.getValue();
+        }
+        return t;
+    }
+
+    /**
+     * 根据bean名称获取指定类型bean
+     * @param name bean名称
+     * @param clazz 返回的bean类型,若类型不匹配,将抛出异常
+     * @return
+     */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getApplicationContext().getBean(name, clazz);
+    }
+
+
     public static Object getBean(String beanName, String className) throws ClassNotFoundException {
         Class clz = Class.forName(className);
         return getApplicationContext().getBean(beanName, clz.getClass());
     }
 
-
+    /**
+     * 判断是否包含bean
+     * @param name
+     * @return
+     */
     public static boolean containsBean(String name) {
         return getApplicationContext().containsBean(name);
     }
 
-
+    /**
+     * 判断bean是否单例
+     * @param name
+     * @return
+     * @throws NoSuchBeanDefinitionException
+     */
     public static boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
         return getApplicationContext().isSingleton(name);
     }
 
-
+    /**
+     * 获取bean的类型
+     * @param name
+     * @return
+     * @throws NoSuchBeanDefinitionException
+     */
     public static Class<?> getType(String name) throws NoSuchBeanDefinitionException {
         return getApplicationContext().getType(name);
     }
