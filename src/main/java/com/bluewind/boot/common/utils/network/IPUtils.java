07fe4255a42dd6291f193631071873586f471d7e
liuxingyu01 <184974699@qq.com>
2022-04-29 15:04:35 +0800

    优化：

--- src/main/java/com/bluewind/boot/common/utils/network/IPUtils.java
+++ src/main/java/com/bluewind/boot/common/utils/network/IPUtils.java
@@ -2,10 +2,12 @@ package com.bluewind.boot.common.utils.network;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+
 /**
  * @author liuxingyu01
  * @date 2021-01-25-13:48
- * @description 获取IP地址
+ * @description 根据HttpServletRequest获取IP地址
  **/
 public class IPUtils {
     /**
@@ -29,6 +31,10 @@ public class IPUtils {
             }
             if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
                 ip = request.getRemoteAddr();
+            }
+            if ("127.0.0.1".equals(ip) || "0:0:0:0:0:0:0:1".equals(ip)) {
+                // 根据网卡取本机配置的IP
+                ip = InetAddress.getLocalHost().getHostAddress();
             }
         } catch (Exception e) {
             e.printStackTrace();
