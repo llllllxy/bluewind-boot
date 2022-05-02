990c18ea6361c36f403b7fcbc05b74e9d72fff00
liuxingyu01 <184974699@qq.com>
2022-05-03 00:20:50 +0800

    增加注释

--- src/main/java/com/bluewind/boot/common/annotation/RequestLimit.java
+++ src/main/java/com/bluewind/boot/common/annotation/RequestLimit.java
@@ -1,15 +1,12 @@
 package com.bluewind.boot.common.annotation;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * @author liuxingyu01
  * @date 2021-03-23-10:39
  * @description one minutes request frequency is Fifty times, exceeding the wait five minutes
+ * 使用方法：@RequestLimit(time = 60, count = 50, waits = 300)
  * 一分钟的请求频率是五十次，超过了，则禁止访问300秒
  **/
 @Target(ElementType.METHOD)
