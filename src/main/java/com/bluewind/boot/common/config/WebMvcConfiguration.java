5d0f29fc9eb2e75cf562ac43190bd4bdd875e8f4
liuxingyu01 <184974699@qq.com>
2022-05-03 00:27:01 +0800

    修改：开放注册等接口不被拦截

--- src/main/java/com/bluewind/boot/common/config/WebMvcConfiguration.java
+++ src/main/java/com/bluewind/boot/common/config/WebMvcConfiguration.java
@@ -58,7 +58,11 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         List<String> excludePathList = new ArrayList<>();
         excludePathList.add("/admin/login"); // 登陆页面
+        excludePathList.add("/admin/register"); // 注册页面
         excludePathList.add("/admin/doLogin"); // 登录接口
+        excludePathList.add("/admin/doRegister"); // 注册接口
+        excludePathList.add("/admin/sendEmail"); // 注册获取邮箱验证码的接口
+
         excludePathList.add("/anon/**");  // 开放itfc和anon接口（itfc和anon开头的可免认证访问）
         excludePathList.add("/itfc/**");  // 开放itfc和anon接口（itfc和anon开头的可免认证访问）
         excludePathList.add("/captcha/**"); // 开放获取行为验验证码接口
