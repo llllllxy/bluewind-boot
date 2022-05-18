7c8cebfa82eaec30c943a2f2ba83dc951d1ddbb7
liuxingyu01 <184974699@qq.com>
2022-05-18 14:51:46 +0800

    格式化代码

--- src/main/java/com/bluewind/boot/common/utils/idtable/IdTableUtils.java
+++ src/main/java/com/bluewind/boot/common/utils/idtable/IdTableUtils.java
@@ -1,10 +1,10 @@
 package com.bluewind.boot.common.utils.idtable;
 
+import com.bluewind.boot.common.consts.SystemConst;
 import com.bluewind.boot.common.utils.RedisUtil;
 import com.bluewind.boot.common.utils.lang.StringUtils;
 import com.bluewind.boot.common.utils.mybatis.MybatisSqlTool;
 import com.bluewind.boot.common.utils.spring.SpringUtil;
-import com.bluewind.boot.common.consts.SystemConst;
 import org.apache.commons.lang3.time.FastDateFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +15,8 @@ import java.util.*;
  * @author liuxingyu01
  * @date 2021-03-28-22:51
  * @description 自定义业务流水号工具类，需要先维护一个，再使用
- *              优化参考美团Leaf，从数据库批量的获取自增ID，每次从数据库取出一个号段范围，例如 (1,100] 代表100个ID，
- *              业务服务将号段在本地生成1~100的自增ID并加载到redis，减少对数据库的频率操作
+ * 优化参考美团Leaf，从数据库批量的获取自增ID，每次从数据库取出一个号段范围，例如 (1,100] 代表100个ID，
+ * 业务服务将号段在本地生成1~100的自增ID并加载到redis，减少对数据库的频率操作
  **/
 public class IdTableUtils {
     final static Logger logger = LoggerFactory.getLogger(IdTableUtils.class);
@@ -26,13 +26,13 @@ public class IdTableUtils {
     private static final int IDTABLE_CACHE_NUMBER = 100;
 
     private static RedisUtil redisUtil;
+
     private static RedisUtil getRedisUtil() {
         if (redisUtil == null) {
             redisUtil = SpringUtil.getBean("redisUtil");
         }
         return redisUtil;
     }
-
 
 
     /**
@@ -57,7 +57,7 @@ public class IdTableUtils {
         // 获取list缓存的长度，判断是否大于0
         if (getRedisUtil().lGetListSize(cacheKey) > 0) {
             Object value = getRedisUtil().lLeftPop(cacheKey);
-            if(value != null){
+            if (value != null) {
                 return value.toString();
             }
         }
@@ -70,6 +70,7 @@ public class IdTableUtils {
 
     /**
      * 批类构造流水号字符串
+     *
      * @param idCode 流水号编码
      * @return List<Object> 流水号列表
      */
@@ -100,7 +101,7 @@ public class IdTableUtils {
     /**
      * 根据规则构造流水号字符串
      *
-     * @param idT 流水号Map
+     * @param idT         流水号Map
      * @param nextIdValue 流水号当前值
      * @return String 流水号字符串
      */
@@ -143,8 +144,8 @@ public class IdTableUtils {
     /**
      * 获取指定位数的数字字符串，不足补0
      *
-     * @param nextIdValue  数字
-     * @param numLen 位数
+     * @param nextIdValue 数字
+     * @param numLen      位数
      * @return
      */
     private static String get0Str(long nextIdValue, int numLen) {
