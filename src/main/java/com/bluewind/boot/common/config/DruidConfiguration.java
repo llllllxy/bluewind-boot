e048cd052d6e385c5c86e54b5c0fee004978336b
liuxingyu01 <184974699@qq.com>
2022-07-16 10:57:16 +0800

    更改druid的配置

--- src/main/java/com/bluewind/boot/common/config/DruidConfiguration.java
+++ src/main/java/com/bluewind/boot/common/config/DruidConfiguration.java
@@ -1,13 +1,9 @@
 package com.bluewind.boot.common.config;
 
 import com.alibaba.druid.spring.boot.autoconfigure.properties.DruidStatProperties;
-import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
 import com.alibaba.druid.util.Utils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,24 +19,6 @@ import java.io.IOException;
  **/
 @Configuration
 public class DruidConfiguration {
-    private static final Logger logger = LoggerFactory.getLogger(DruidConfiguration.class);
-
-
-    @Bean
-    public ServletRegistrationBean druidServlet() {
-        logger.info("init Druid Servlet Configuration ");
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
-        // IP白名单
-//        servletRegistrationBean.addInitParameter("allow", "*");
-        // IP黑名单(共同存在时，deny优先于allow)
-//        servletRegistrationBean.addInitParameter("deny", "192.168.1.100");
-        //控制台管理用户
-        servletRegistrationBean.addInitParameter("loginUsername", "admin"); // 用户名
-        servletRegistrationBean.addInitParameter("loginPassword", "123456"); // 密码
-        //是否能够重置数据 禁用HTML页面上的“Reset All”功能
-        servletRegistrationBean.addInitParameter("resetEnable", "false");
-        return servletRegistrationBean;
-    }
 
     @Bean
     public FilterRegistrationBean filterRegistrationBean() {
