85267ba86321b58b373f2fd0d6342313fafc9506
liuxingyu01 <184974699@qq.com>
2023-01-04 22:21:04 +0800

    通用数据集工具-实体类使用时间转换

--- src/main/java/com/bluewind/boot/module/tool/dataset/generalexport/entity/Dataset.java
+++ src/main/java/com/bluewind/boot/module/tool/dataset/generalexport/entity/Dataset.java
@@ -1,7 +1,9 @@
 package com.bluewind.boot.module.tool.dataset.generalexport.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.io.Serializable;
-import java.util.Date;
 
 /**
  * @author liuxingyu01
@@ -14,12 +16,16 @@ public class Dataset implements Serializable {
     /**
      * 更新时间
      */
-    private Date updateTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private String updateTime;
 
     /**
      * 创建时间
      */
-    private Date createTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private String createTime;
 
     /**
      * 数据集id(主键id)
@@ -41,19 +47,19 @@ public class Dataset implements Serializable {
      */
     private String note;
 
-    public Date getUpdateTime() {
+    public String getUpdateTime() {
         return updateTime;
     }
 
-    public void setUpdateTime(Date updateTime) {
+    public void setUpdateTime(String updateTime) {
         this.updateTime = updateTime;
     }
 
-    public Date getCreateTime() {
+    public String getCreateTime() {
         return createTime;
     }
 
-    public void setCreateTime(Date createTime) {
+    public void setCreateTime(String createTime) {
         this.createTime = createTime;
     }
 
