eecfec01252a7bc4233c3158d6fda6a597b0a382
17862719591 <184974699@qq.com>
2023-02-06 11:13:27 +0800

    更新之前描述不正确的注释

--- src/main/java/com/bluewind/boot/common/annotation/DataSourceWith.java
+++ src/main/java/com/bluewind/boot/common/annotation/DataSourceWith.java
@@ -8,14 +8,15 @@ import java.lang.annotation.*;
  * @author liuxingyu01
  * @date 2021-03-10-10:41
  * @description 数据源切换注解
- * 使用方法 ： 加上@RoutingWith("slaveDataSource")注解即可切换到第二数据源
+ * 使用方法 ： 加上@DataSourceWith(DataSourceType.SLAVE)注解即可切换到第二数据源
  **/
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface DataSourceWith {
+
     /**
      * 切换数据源名称
      */
-    public DataSourceType value() default DataSourceType.MASTER;
+    DataSourceType value() default DataSourceType.MASTER;
 }
